e05a685e456c36693e12f1313877c2024ec79d28
Ahmad Sidik Rudini <sidikrudini16@gmail.com>
2023-01-26 10:35:21 +0700

    update cv link

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -272,7 +272,7 @@ export const initialState = {
       linkedin: "https://www.linkedin.com/in/asidikrdn/",
       codepen: "https://codepen.io/asidikrdn",
     },
-    cv: "https://drive.google.com/file/d/1kAdjeiigDOOOsNUPWRJ43Lr4s8jtsNwH/view?usp=share_link",
+    cv: "https://drive.google.com/file/d/1CKcckpWRhxaw3G6t_qDhIDTaUb2h9KzF/view?usp=share_link",
   },
   pagePosition: "intro",
 };
