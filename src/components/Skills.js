13b2510e9d303f28f0488302db11011f18666fac
Ahmad Sidik Rudini <sidikrudini16@gmail.com>
2023-09-05 15:11:42 +0700

    update intro, education, and skills certificate

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -12,51 +12,14 @@ const Skills = () => {
 
   return (
     <>
-      <Container fluid id="skills" className="py-2">
-        <h1 className="display-2 fw-bold judulHalaman text-center">Skills</h1>
-        <Row>
-          {state.userData.skills.map((skill) => {
-            return (
-              <Col xs={12} lg={6} className="px-lg-5 py-2">
-                <Card
-                  className="mx-2 rounded-5 border-success"
-                  style={{ boxShadow: "3px 3px 5px 1px #005D66" }}
-                >
-                  <Card.Body>
-                    <Card.Title>
-                      <h4 className="text-center">{skill.role}</h4>
-                      <hr className="w-75 mx-auto text-success" />
-                    </Card.Title>
-                    <Card.Text>
-                      <ul>
-                        {skill.tech.map((techData) => {
-                          return Array.isArray(techData) ? (
-                            <ul>
-                              {techData.map((subTech) => {
-                                return <li>{subTech}</li>;
-                              })}
-                            </ul>
-                          ) : (
-                            <li>{techData}</li>
-                          );
-                        })}
-                      </ul>
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
-            );
-          })}
-        </Row>
-      </Container>
       <Container fluid id="cert" className="py-2">
         <h1 className="display-2 fw-bold judulHalaman text-center">
-          Certificates
+          Skills & Certificates
         </h1>
         <Row>
           {state.userData.certificates.map((cert) => {
             return (
-              <Col xs={12} md={6} className="p-3">
+              <Col key={cert.id} xs={12} md={6} className="p-3">
                 <Card className="rounded-3 border border-top-0 border-bottom-0 border-end-0 border-info border-5 h-100">
                   <Card.Body className="pt-0 pb-1">
                     <Card.Title className="h4">{cert.title}</Card.Title>
